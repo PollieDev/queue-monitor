c39234970f51ae1ab81948845b22b82d062caa5a
PieterJanJT <pieterjan@jobtoolz.com>
2023-02-19 22:47:16 +0100

    Remove unneeded comment

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -2,7 +2,6 @@
 <html lang="en" class="h-full bg-gray-200">
 
 <head>
-    <!-- Don't forget to use your package name -->
     <link rel="stylesheet" href="{{ mix('css/app.css', 'vendor/queue_monitor') }}">
     <script src="{{ mix('js/app.js', 'vendor/queue_monitor') }}" defer></script>
 </head>
